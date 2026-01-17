559e9e5ddb3b0aff81f4a14551d10e4033e753ca
Aryaman-Saraf <aryamansaraf12@gmail.com>
2026-01-18 01:00:11 +0530

    Changes

--- generator.py
+++ generator.py
@@ -2,21 +2,21 @@ from itertools import product
 
 # ---------- BASE DATA ----------
 BASE_ITEMS = [
-    ("banana_peel", "organic", 1),
-    ("apple_core", "organic", 1),
-    ("mango_peel", "organic", 1),
-    ("orange_peel", "organic", 1),
-    ("plastic_bottle", "plastic_recyclable", 2),
-    ("plastic_wrapper", "plastic_non_recyclable", 3),
-    ("paper_cup", "paper", 2),
-    ("cardboard_box", "paper", 2),
-    ("glass_bottle", "glass", 4),
-    ("broken_glass", "glass", 5),
-    ("used_battery", "e_waste", 6),
-    ("mobile_charger", "e_waste", 5),
-    ("sanitary_pad", "sanitary", 7),
-    ("diaper", "sanitary", 7),
-    ("cloth_piece", "cloth", 3),
+    ("banana_peel", "organic - Green Dustbin", 1),
+    ("apple_core", "organic - Green Dustbin", 1),
+    ("mango_peel", "organic - Green Dustbin", 1),
+    ("orange_peel", "organic - Green Dustbin", 1),
+    ("plastic_bottle", "plastic_recyclable - Blue Dustbin", 2),
+    ("plastic_wrapper", "plastic_non_recyclable - Black or Grey Dustbin", 3),
+    ("paper_cup", "paper - Blue Dustbin", 2),
+    ("cardboard_box", "paper - Blue Dustbin", 2),
+    ("glass_bottle", "glass - Blue or Green Dustbin", 4),
+    ("broken_glass", "glass - Blue or Green Dustbin", 5),
+    ("used_battery", "e_waste - Green, Yellow, or Grey Dustbin", 6),
+    ("mobile_charger", "e_waste - Green, Yellow, or Grey Dustbin", 5),
+    ("sanitary_pad", "sanitary - yellow, black/tiger-striped, white, or blue dustbin", 7),
+    ("diaper", "sanitary - yellow, black/tiger-striped, white, or blue dustbin", 7),
+    ("cloth_piece", "cloth - Green Dustbin", 3),
 ] * 9  # 15 × 9 = 135 base items
 
 CONDITIONS = {
