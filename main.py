2ff577ea2e8f393b20771cabb9d93e2f714ef31f
Aryaman-Saraf <aryamansaraf12@gmail.com>
2026-01-18 01:11:34 +0530

    Added a temporary database using dictionaries and accessing them using binary search

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 '''This is main.py'''
 from internetAvailablity import internet_Available
 from translator import *
+from database import bin
+
+def binary_search(bin , target):
+    """
+    sorted_dict : pre-sorted dictionary (by keys)
+    target      : item name to search (string)
+    returns     : value dict if found, else None
+    """
+
+    keys = list(bin.keys())  # already sorted once
+
+    left = 0
+    right = len(keys) - 1
+
+    target = target.lower()
+
+    while left <= right:
+        mid = (left + right) // 2
+        mid_key = keys[mid].lower()
+
+        if mid_key == target:
+            return bin[keys[mid]]
+
+        elif mid_key < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return None
 
 if(internet_Available()):
     print("All Langauges Supported (Try Avoiding Hinglish)")
 else:
     print('''Only english supported offline''')
     
-t = input("\n\nEnter waste name only - ")
-t= languageToEnglish(t)
-print(t)
+text = input("\n\nEnter waste name only - ")
+textTranslated= languageToEnglish(text)
+
+result=binary_search(bin,textTranslated)
+if result:
+    print(
+        f"✅ Item: {text}\n"
+        f"   Category: {result['Item']}\n"
+        f"   Bin: {result['Bin']}"
+    )
+else:
+    print(f"❌ Item '{text}' not found in database")
