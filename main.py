9358d0041377173f2572f2884fe1256b18dcfbb7
Aryaman-Saraf <aryamansaraf12@gmail.com>
2026-01-17 23:28:36 +0530

    Added online translation only - Hinglish supported but not always correct

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 '''This is main.py'''
-
+from internetAvailablity import internet_Available
 from translator import *
-t = input("Enter text - ")
+
+if(internet_Available()):
+    print("All Langauges Supported (Try Avoiding Hinglish)")
+else:
+    print('''Only english supported offline''')
+    
+t = input("\n\nEnter waste name only - ")
 t= languageToEnglish(t)
 print(t)
